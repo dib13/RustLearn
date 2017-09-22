d1a9e18889cc7d4a466e89247abecd79d04bf92c
dib13 <31906180+dib13@users.noreply.github.com>
2017-09-22 09:54:59 -0400

    Finally got temp working!

--- chapter3-temp/src/main.rs
+++ chapter3-temp/src/main.rs
@@ -1,13 +1,15 @@
 // This is a program to convert Fahrenheit to Celsius and vice-versa.
 use std::io;
 
-fn fahrenheit_to_celsius(ftemp: f64) -> f64 {
-    let ctemp = (ftemp - 32.0) / 1.8;
+fn fahrenheit_to_celsius(ftemp: u32) -> f64 {
+    let work = ftemp as f64;
+    let ctemp = (work - 32.0) / 1.8;
     ctemp
 }
 
-fn celsius_to_fahrenheit(ctemp: f64) -> f64 {
-    let ftemp = ctemp * 1.8 + 32.0;
+fn celsius_to_fahrenheit(ctemp: u32) -> f64 {
+    let work = ctemp as f64;
+    let ftemp = work * 1.8 + 32.0;
     ftemp
 }
 
@@ -19,14 +21,14 @@ fn main() {
         .read_line(&mut temp_num)
         .expect("Failed to read line");
 
-    let temp_num = match temp_num.trim().parse() {
+    let temp_num : u32 = match temp_num.trim().parse() {
         Ok(num) => num,
-        Err(_) => println!("Not a valid number?"),
+        Err(_) => 100,
     };
 
+    let tempc = fahrenheit_to_celsius(temp_num);
+    let tempf = celsius_to_fahrenheit(temp_num);
 
-    let temp_num = { fahrenheit_to_celsius(temp_num) };
-    let temp_num = { temp_num.to_string() };
-
-    println!("{}", temp_num);
+    println!("Your temperature in F is: {}", tempf);
+    println!("Your temperature in C is: {}", tempc);
 }
--- chapter3-temp/target/debug/deps/chapter3_temp-431be05b9168a041.d
+++
@@ -1,5 +0,0 @@
-H:\Programming\Rust\RustLearn\chapter3-temp\target\debug\deps\chapter3_temp-431be05b9168a041.exe: src\main.rs
-
-H:\Programming\Rust\RustLearn\chapter3-temp\target\debug\deps\chapter3_temp-431be05b9168a041.d: src\main.rs
-
-src\main.rs:
